7b2b1672d82feedc6b6b7c6a6defa575d0532231
Angadjit <Angad38.agcs@gmail.com>
2022-05-02 20:08:06 +0530

    created movie tile ui

--- movie_details_app/app/src/main/java/com/team/moviedetailsapp/MainActivity.java
+++ movie_details_app/app/src/main/java/com/team/moviedetailsapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.team.moviedetailsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.team.moviedetailsapp.logic.MoviesListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView movies = findViewById(R.id.moviesList);
+        MoviesListAdapter moviesListAdapter = new MoviesListAdapter(getApplicationContext());
+        movies.setLayoutManager(new LinearLayoutManager(this));
+        movies.setAdapter(moviesListAdapter);
     }
 }
--- movie_details_app/app/src/main/java/com/team/moviedetailsapp/data/dataprovider/MoviesApi.java
+++ movie_details_app/app/src/main/java/com/team/moviedetailsapp/data/dataprovider/MoviesApi.java
@@ -3,7 +3,7 @@ package com.team.moviedetailsapp.data.dataprovider;
 public class MoviesApi {
 
     private void getBody(){
-        
+
     }
 
     public void getRawMovies(){
