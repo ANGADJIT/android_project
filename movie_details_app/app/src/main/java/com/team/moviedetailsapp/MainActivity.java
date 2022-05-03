b4e56240a4d01e6ba9c7a3114460f61b1b7c789b
Angadjit <Angad38.agcs@gmail.com>
2022-05-03 16:10:18 +0530

    added search feature and archritectured the app

--- movie_details_app/app/src/main/java/com/team/moviedetailsapp/MainActivity.java
+++ movie_details_app/app/src/main/java/com/team/moviedetailsapp/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.team.moviedetailsapp.logic.MoviesListAdapter;
 
@@ -15,9 +19,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initializing RecyclerView
         RecyclerView movies = findViewById(R.id.moviesList);
         MoviesListAdapter moviesListAdapter = new MoviesListAdapter(getApplicationContext());
         movies.setLayoutManager(new LinearLayoutManager(this));
         movies.setAdapter(moviesListAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Search For Movie");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
